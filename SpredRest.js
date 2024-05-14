bdcc4c48c19da054d2c6a4804742e8e7b2a9bf3b
PreetS123 <preetbhardwaj517@gmail.com>
2024-05-14 23:21:44 +0530

    "rest operator example"

--- SpredRest.js
+++ SpredRest.js
@@ -1,16 +1,20 @@
-
-
 // Spred operator--> When we need to access value one by one.
 // Rest operator--> When we have multiple inputs and we don't know the exact number of inputs then we use rest operator.
 
-
-function num(...nums){ // here rest operator is being used
-    let multiply=1;
-    for(let i=0; i<nums.length; i++){
-        multiply*=nums[i];
-    }
-    return multiply;
+function num(...nums) {
+  // here rest operator is being used
+  let multiply = 1;
+  for (let i = 0; i < nums.length; i++) {
+    multiply *= nums[i];
+  }
+  return multiply;
 }
-let arr=[10,2,3,9];
+let arr = [10, 2, 3, 9];
 
-console.log(num(...arr)); // here spread operator is being used
+// console.log(num(...arr)); // here spread operator is being used
+
+function fun(a, b, c, ...number) {
+  // rest operator must be as last parameter.
+  console.log(a, b, number);
+}
+fun(2, 3, 4, 2, 88, 8, 9, 7, 4, 3, 2, 1);
