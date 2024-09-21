50c9c166c443b9a73b0db32d20c0cf224fb94016
PreetS123 <preetbhardwaj517@gmail.com>
2024-09-21 18:53:13 +0530

    settimeout question

---
+++ settimeout.js
@@ -0,0 +1,17 @@
+/**
+ * What is the setTimeout function in JavaScript, and how does it work?
+
+
+ * How does JavaScript handle asynchronous code execution with setTimeout?
+
+ * What is the difference between setTimeout and setInterval?
+
+ * What happens if you pass a string to setTimeout as the first argument instead of a function?
+
+
+ * How can you cancel a setTimeout? Provide an example.
+  
+   let timer = setTimeout(() => console.log('This will not be logged'), 5000);
+   clearTimeout(timer);
+
+ */
